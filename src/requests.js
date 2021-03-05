9be0c2e3039c8457c41ecbc11e614ccd650c7d34
GCAdigwe <gcadigwe@gmail.com>
2021-03-05 12:44:37 +0000

    started working on row, finished setting up api endpoints for requests

---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const APIKEY = "3498e629c462a5ac11c28ae2b036b23a";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${APIKEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${APIKEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${APIKEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${APIKEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${APIKEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${APIKEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${APIKEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${APIKEY}&with_genres=99`,
+};
+
+export default requests;
